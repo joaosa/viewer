f3362c7abaa8408fdb23c01fd2dfba27f86074d7
Joao Andrade <jjadonotenter@gmail.com>
2013-03-07 09:27:15 +0000

    added livereload; cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,6 @@ module.exports = function(grunt) {
     },
     simplemocha: {
       options: {
-        globals: ['should'],
         uid: 'bdd',
         reporter: 'nyan'
       },
@@ -30,13 +29,13 @@ module.exports = function(grunt) {
     },
     browserify: {
       'dist/bundle.js': {
-        requires: ['./lib/viewer.js'],
+        requires: ['./lib/viewer.js']
         // entries: ['lib/**/*.js']
       }
     },
     uglify: {
       options: {
-        // banner: grunt.file.read('LICENCE-MIT'),
+        banner: grunt.file.read('LICENSE-MIT'),
         mangle: { toplevel: true },
         squeeze: { dead_code: false },
         codegen: { quote_keys: true }
@@ -47,30 +46,46 @@ module.exports = function(grunt) {
         }
       }
     },
+    watchFiles: ['lib/**/*.js', 'test/**/*.js', 'index.html', 'Gruntfile.js'],
     watch: {
-      files: ['**/lib/*', 'index.html', 'Gruntfile.js'],
+      files: '<%= watchFiles %>',
       tasks: ['default']
     },
     connect: {
-      server: {
+      livereload: {
         options: {
-          base: '.',
-          keepalive: true
+          port: 9001,
+          middleware: function(connect) {
+            return [lrSnippet, folderMount(connect, '.')];
+          }
         }
       }
+    },
+    regarde: {
+      default: {
+        files: '<%= watchFiles %>',
+        tasks: ['livereload']
+      }    
     }
   });
   
+  var path = require('path');
+  var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
+
+  var folderMount = function folderMount(connect, point) {
+    return connect.static(path.resolve(point));
+  };
+
   grunt.registerTask('server', 'Start a custom server.', function() {
     var done = this.async();
-    var port = 9000;
+    var port = 9800;
     grunt.log.writeln('Starting a web server on port ' + port);
 
     var io = require('socket.io').listen(port).on('close', done);    
     
     io.sockets.on('connection', function(socket) {
-      var recursive = function () {
-          socket.emit('stream', {"stream": [{"type": "node", "id": "H"}/*, {"type": "link", "source": "H", "target": "A"}*/]});
+      var recursive = function() {
+          socket.emit('stream', {"stream": [{"type": "node", "id": "H"}, {"type": "link", "source": "H", "target": "A"}]});
           setTimeout(recursive, 1000);
       };
       recursive();
@@ -107,8 +122,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-regarde');
+  grunt.loadNpmTasks('grunt-contrib-livereload');
 
   // Default task.
-  grunt.registerTask('default', [/*'jshint',*/ 'simplemocha', 'browserify'/*, 'uglify'*/]);
+  grunt.registerTask('default', [/*'jshint',*/ 'simplemocha', 'browserify'/*, 'uglify'*/, 'livereload-start', 'connect', 'regarde']);
 
 };
